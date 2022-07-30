45dfbce82384380571af5aea58cde7052a7d0f53
eduardo <A01704641@itesm.mx>
2022-07-29 23:34:46 -0500

    deploy settings

--- deploy.sh
+++ deploy.sh
@@ -10,5 +10,5 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:angieanaya/portfolio-update.git master:gh-pages
+git push -f git@github.com:jeduardocb/finanzasPersonales.git master:gh-pages
 cd -
