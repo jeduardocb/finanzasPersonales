f76051fd660d1aa7cf0cf9f339e2987ac5732081
eduardo <A01704641@itesm.mx>
2022-07-29 21:57:46 -0500

    finanzas personales terminado

--- src/views/FinanzasPersonales.vue
+++ src/views/FinanzasPersonales.vue
@@ -2,9 +2,23 @@
   <Title
     key="01"
     title="Finanzas Personales"
-    description="Son las finanzas relacionadas con la capacidad de ahorrar por parte de los individuos, así como obtener recursos financieros adicionales procedentes, además engloban las decisiones de inversión de los excedentes de recursos financieros disponibles."
+    description="Se refieren al manejo y administración de dinero y bienes de un individuo o familia, esto con el objetivo de alcanzar una satisfacción económica."
   />
+
   <div class="flex flex-col gap-8 -topbar">
+    <h3 class="font-theme_bold text-2xl text-head_text">
+      Componentes de una planeación personal.
+    </h3>
+    <ul class="list-disc">
+      <li class="pl-4">Obtener recursos financieros.</li>
+      <li class="pl-4">Planificación de tu presupuesto.</li>
+      <li class="pl-4">Ahorros para gastos inesperados.</li>
+      <li class="pl-4">Obtener un crédito de manera inteligente.</li>
+      <li class="pl-4">Gastos para cubrir necesidades diarias.</li>
+      <li class="pl-4">Controlar riesgos por medio de un seguro.</li>
+      <li class="pl-4">Inversiones a largo plazo para un buen futuro.</li>
+      <li class="pl-4">Pensar en el retiro y en activos duraderos.</li>
+    </ul>
     <MainProjectItem
       key="Créditos"
       name="Créditos"
@@ -39,6 +53,54 @@
         >Calculadora de créditos</ActionButton
       >
     </div>
+    <MainProjectItem
+      key="Créditos"
+      name="Amortización"
+      description="En este método se realizan una serie de pagos periódicos; cuando estos pagos son iguales, el caso habitual, su tamaño puede determinarse mediante los procedimientos de anualidad."
+    />
+    <h3 class="font-theme_bold text-2xl text-head_text">
+      Tipos de amortización
+    </h3>
+    <ul class="list-disc">
+      <li class="pl-4">
+        <span class="font-bold"> Francesa: </span>Pagos iguales.
+      </li>
+      <li class="pl-4">
+        <span class="font-bold">Alemana: </span>Amortización compuesta (pagos a
+        capital) , intereses más bajos.
+      </li>
+      <li class="pl-4">
+        <span class="font-bold">Americana:</span> Pagos de intereses durante la
+        vida del crédito, pago de capital al final .
+      </li>
+    </ul>
+
+    <h3 class="font-theme_bold text-2xl text-head_text">
+      Tipos de perfiles de gastos
+    </h3>
+    <ul class="list-disc">
+      <li class="pl-4">
+        <span class="font-bold"> Saver: </span>El sabe que ahorrar es un deber,
+        tienen disciplina para esto.
+      </li>
+      <li class="pl-4">
+        <span class="font-bold">Spender: </span>Lo más imporante es vivir el día
+        a día, las personas se deben de dar cuenta cuanto tengo
+      </li>
+      <li class="pl-4">
+        <span class="font-bold">No control:</span> No tienen idea de cuanto
+        ganaron ni cuanto gastaron.
+      </li>
+      <li class="pl-4">
+        <span class="font-bold">Distracted:</span> Gasta menos de lo que tiene,
+        aún así no saben cuanto ahorraron en algunas ocasiones.
+      </li>
+      <li class="pl-4">
+        <span class="font-bold">Financial:</span> Cuentan con nu budget, control
+        de gastos, saben lo que significan las inversiones y usan su dinero
+        inteligentemente.
+      </li>
+    </ul>
   </div>
 </template>
 <script>
